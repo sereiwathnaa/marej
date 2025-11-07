bbad5c70de630da1d5dc6e02d56a1899b8a8896a
Nayuki-Sunder <sereiwathnaros@chungbuk.ac.kr>
2025-11-07 14:23:55 +0900

    kvcaching test on gpt2 with GPTTokenizer

--- src/models/gpt/sample_bpe.py
+++ src/models/gpt/sample_bpe.py
@@ -20,8 +20,8 @@ start = """
 CHORUS
 HENRY V, KING OF ENGLAND
 """ # or "<|endoftext|>" or etc. Can also specify a file, use as: "FILE:prompt.txt"
-num_samples = 10 # number of samples to draw
-max_new_tokens = 500 # number of tokens generated in each sample
+num_samples = 3 # number of samples to draw
+max_new_tokens = 100 # number of tokens generated in each sample
 temperature = 0.8 # 1.0 = no change, < 1.0 = less random, > 1.0 = more random, in predictions
 top_k = 200 # retain only the top_k most likely tokens, clamp others to have 0 probability
 seed = 1337
