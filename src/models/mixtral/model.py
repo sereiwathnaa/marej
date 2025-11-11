02e66dab6f94cc0000d34fdba38dc99f3e28c7ee
Nayuki-Sunder <sereiwathnaros@chungbuk.ac.kr>
2025-11-11 15:32:07 +0900

    Add MOE, FFN

--- src/models/mixtral/model.py
+++ src/models/mixtral/model.py
@@ -1,7 +1,8 @@
 import torch
 from torch import nn, Tensor
+import torch.nn.functional as F
 
-
+from einops import repeat
 from src.layers.attention import GroupedQueryRotaryAttention
 from src.layers.normalization import RMSNorm
 
@@ -67,3 +68,46 @@ class DecoderBlock(nn.Module):
         self.moe = MOE(embed_dim, ffn_hidden_dim, n_experts, n_experts_per_tok)
 
         pass
+
+class MOE(nn.Module):
+    def __init__(self,
+                 input_dim: int,
+                 ffn_hidden_dim: int,
+                 n_experts: int,
+                 n_experts_per_tok: int):
+        super().__init__()
+        self.experts = nn.ModuleList(
+            [FeedForwardBlock(input_dim, ffn_hidden_dim) for _ in range(n_experts)]
+        )
+        self.gate = nn.Linear(input_dim, n_experts, bias=False)
+        self.n_experts_per_tok = n_experts_per_tok
+    
+    def forward(self, x):
+        # x : (batch, seqlen, embed_dim)
+        logits = self.gate(x) # (batch, seqlen, n_experts)
+
+        (weights, selected_experts) = torch.topk(logits, self.n_experts_per_tok, dim=-1) # (batch, seqlen, n_experts_per_tok)
+        weights = F.softmax(weights, dim=-1)
+
+        x_repeat = repeat(x, "b l d -> b l exps d", exps=self.n_experts_per_tok)
+        output = torch.empty_like(x_repeat)
+        for i, expert in enumerate(self.experts):
+            mask = (selected_experts == i)
+            output[mask] = expert(x_repeat[mask]) * weights[mask][None, :]
+        
+        output = output.sum(dim=2)
+        return output
+
+class FeedForwardBlock(nn.Module):
+    def __init__(self,
+                 input_dim,
+                 ffn_hidden_dim):
+        super().__init__()
+        self.linear1 = nn.Linear(input_dim, ffn_hidden_dim)
+        self.linear2 = nn.Linear(ffn_hidden_dim, input_dim)
+        self.linear3 = nn.Linear(input_dim, ffn_hidden_dim)
+
+    def forward(self, x):
+        x = F.silu(self.linear1(x)) * self.linear3(x)
+        x = self.linear2(x)
+        return x
